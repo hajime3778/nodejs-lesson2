4ef7ad74ac0ed8dc77c5014e0a5bef988cce1921
hajime.takeuchi <shadowthe8156@gmail.com>
2025-05-08 21:37:28 +0900

    add express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!!!");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const server = app.listen(3000, function() {
+  console.log("Node.js is listening to PORT:" + server.address().port);
+})
+
+app.disable("x-powered-by");
+app.use(cors()).use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    id: "1",
+    name: "hogehoge",
+    description: "hello world",
+  });
+});
